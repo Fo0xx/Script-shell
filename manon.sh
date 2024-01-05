d90618d716c3fe898f76ff26bd29ad4d94a7c454
Valodes <darkraken@live.fr>
2024-01-05 11:00:09 +0100

    :sparkles: Couleurs et lisibilité
    Ajouts de couleurs dans la console et rendu la lisibilité du code meilleure

--- manon.sh
+++ manon.sh
@@ -2,8 +2,8 @@
 
 # Vérifier si Git est installé
 if ! command -v git &> /dev/null; then
-        echo "Git n'est pas installé. Veuillez installer Git."
-        exit 1
+    echo "$(tput setaf 1)Git n'est pas installé. Veuillez installer Git.$(tput sgr0)"
+    exit 1
 fi
 
 # Analyser les arguments de ligne de commande
@@ -19,11 +19,11 @@ while getopts ":w:r:s:" opt; do
             delete_files=$OPTARG
             ;;
         \? )
-            echo "Option invalide : $OPTARG" 1>&2
+            echo "$(tput setaf 1)Option invalide : $OPTARG$(tput sgr0)" 1>&2
             exit 1
             ;;
         : )
-            echo "Option invalide : $OPTARG nécessite un argument" 1>&2
+            echo "$(tput setaf 1)Option invalide : $OPTARG nécessite un argument$(tput sgr0)" 1>&2
             exit 1
             ;;
     esac
@@ -32,39 +32,58 @@ shift $((OPTIND -1))
 
 # Valeurs par défaut
 workspace=${workspace:-.}
-refresh_duration=${refresh_duration:-300}
-delete_files=${delete_files:-false}
+refresh_duration=${refresh_duration:-30}
+delete_files=${delete_files:-true}
 
 # Fonction pour vérifier si des fichiers ont été modifiés
 check_modified_files() {
-        cd $workspace
-        modified_files=$(git diff --name-only)
-        if [ -z "$modified_files" ]; then
-                echo "Aucun fichier modifié."
-        else
-                echo "Fichiers modifiés trouvés :"
-                for file in $modified_files; do
-                        modified_lines=$(git diff --numstat $file | awk '{print $1}')
-                        if [ "$modified_lines" -gt 500 ]; then
-                                echo -e "\e[31m$file ($modified_lines lignes modifiées)\e[0m"  # Rouge
-                        elif [ "$modified_lines" -gt 100 ]; then
-                                echo -e "\e[34m$file ($modified_lines lignes modifiées)\e[0m"  # Bleu
-                        else
-                                echo -e "\e[32m$file ($modified_lines lignes modifiées)\e[0m"  # Vert
-                        fi
-                done
-        fi
+    cd $workspace
+    modified_files=$(git diff --name-only)
+    if [ -z "$modified_files" ]; then
+        echo "$(tput setaf 2)Aucun fichier modifié.$(tput sgr0)"
+    else
+        echo "$(tput setaf 3)Fichiers modifiés trouvés :$(tput sgr0)"
+        for file in $modified_files; do
+            modified_lines=$(git diff --numstat $file | awk '{print $1}')
+            if [ "$modified_lines" -gt 200 ]; then
+                echo -e "$(tput setaf 1)$file ($modified_lines lignes modifiées)$(tput sgr0)"  # Rouge
+            elif [ "$modified_lines" -gt 50 ]; then
+                echo -e "$(tput setaf 4)$file ($modified_lines lignes modifiées)$(tput sgr0)"  # Bleu
+            else
+                echo -e "$(tput setaf 2)$file ($modified_lines lignes modifiées)$(tput sgr0)"  # Vert
+            fi
+        done
+    fi
 }
 
 # Exécuter le script en boucle toutes les $refresh_duration secondes
-warnings=0
-while true; do
+count=$refresh_duration
+echo "+--------------------------------------------------------+"
+echo "|                                                        |"
+echo "| $(tput setaf 6)Récapitulatif des arguments choisis :$(tput sgr0)                  |"
+echo "| $(tput setaf 6)Workspace : $workspace$(tput sgr0)"
+echo "| $(tput setaf 6)Durée de rafraîchissement : $refresh_duration secondes$(tput sgr0)"
+echo "| $(tput setaf 6)Supprimer les fichiers modifiés : $delete_files$(tput sgr0)"
+echo "|                                                        |"
+echo "+--------------------------------------------------------+"
+echo ""
+
+while [ $count -gt 0 ]; do
+    if [ $count -eq $refresh_duration ]; then
         check_modified_files
-        warnings=$((warnings+1))
-        if [ "$warnings" -ge 3 ] && [ "$delete_files" = true ]; then
-                # Supprimer tous les fichiers modifiés
-                git ls-files --modified | xargs -d '\n' rm -f
-                warnings=0
+    fi
+    echo -ne "$(tput setaf 1)Compte à rebours : $count$(tput sgr0)\033[0K\r"
+    count=$((count-1))
+    if [ "$count" -eq 0 ] && [ "$delete_files" = true ]; then
+        # Vérifier si des fichiers ont été ajoutés à l'index Git
+        staged_files=$(git diff --name-only --cached)
+        if [ -z "$staged_files" ]; then
+            echo "$(tput setaf 1)Aucun fichier ajouté à l'index Git. Les fichiers modifiés seront supprimés.$(tput sgr0)"
+            git ls-files --modified | xargs -d '\n' rm -f
+        else
+            echo "$(tput setaf 3)Des fichiers ont été ajoutés à l'index Git. Les fichiers modifiés ne seront pas supprimés.$(tput sgr0)"
         fi
-        sleep $refresh_duration
+        count=$refresh_duration
+    fi
+    sleep 1
 done
